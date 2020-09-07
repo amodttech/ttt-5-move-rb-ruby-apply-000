ca0f415b26697133123ba5f70c3aa87e568ef062
Aaron Amodt <aaron@amodttech.com>
2020-09-07 22:40:31 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,14 +14,4 @@ def move(array, index, "X")
   array[index] = "X"
 end
 
-
-
-
-
-
-
-def total(subtotal, tax, discount)
-  subtotal + tax - discount
-end
-
-total(100, 10, 5) # => 105
+move(board,input, "X")
